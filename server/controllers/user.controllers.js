3511221c316203901543739331d559f37ee96509
vivrehal <dhimanvivek777@gamil.com>
2024-02-04 17:44:07 +0530

    added height and weight field in user schema

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -2,9 +2,24 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser=asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        messgae:"ok"
-    })
+    // get data from frontend
+    //validate
+    //check if user already exists
+    //create user
+    //save in db
+    const {
+        username, 
+        name, 
+        email, 
+        password, 
+        dob, 
+        weigt, 
+        height} = req.body;
+    
+    console.log(username, name)
+    
+    
+
 })
 
 export {registerUser}
