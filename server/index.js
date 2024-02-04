68f7190c8528f7d32042400518f7dd9ef82b2c40
vivrehal <dhimanvivek777@gamil.com>
2024-02-04 15:15:59 +0530

    added required packages and mounting middlewares

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
-import express from "express";
+import { app } from "./app.js";
 import connectTodb from "./db.js";
 import 'dotenv/config';
-const app=express();
-app.use(express.json())
-
-connectTodb(process.env.DB_URI);
 
 
-try {
-    app.listen(process.env.PORT,()=>{
-        console.log(`App listening on port ${process.env.PORT}`);
-    })
-    
-} catch (error) {
-    console.log(error);
-    throw error;
-}
+
+connectTodb(process.env.DB_URI).then(()=>{
+    try {
+        app.listen(process.env.PORT || 9000,()=>{
+            console.log(`App listening on port ${process.env.PORT}`);
+        })
+        
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}).catch((err)=>{
+    console.log("DB connection failed !! ", err)
+})
+
+
+
