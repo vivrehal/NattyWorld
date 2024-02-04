d006699d23f416e33b3e0a51ed7012012cbdf4c1
gargmanik6080 <gargmanik6080@gmail.com>
2024-02-04 11:35:15 +0530

    workout name coulumn added

--- server/models/workout.modal.js
+++ server/models/workout.modal.js
@@ -2,6 +2,11 @@ const mogoose = require("mongoose");
 
 
 const workoutSchema = new mogoose.Schema({
+    name : {
+        type: String,
+        required: true,
+        unique: true,
+    },
     plan: {
         type : String, // Link of the pdf of the diet
         unique : true
