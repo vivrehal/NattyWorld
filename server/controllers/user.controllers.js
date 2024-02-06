493e08925374401216e7116b35c4733e5b4e452b
vivrehal <dhimanvivek777@gamil.com>
2024-02-06 15:24:58 +0530

    adding refresh_token route

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -4,7 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { apiError } from "../utils/apiError.js"
 import generateTokens from "../utils/tokenGenerator.js";
 import options from "../utils/cookieOptions.js";
-import { User } from "lucide-react";
+import jwt from "jsonwebtoken"
 
 const registerUser=asyncHandler(async(req,res)=>{
     // get data from frontend
@@ -146,6 +146,43 @@ const logoutUser= asyncHandler(async(req,res)=>{
     .json(
         new ApiResponse(200,{},"User Logged out")
     )
+})
+
+const newTokenOnExpiry=asyncHandler(async(req,res)=>{
+    const token=req.cookies?.refreshToken
+    if(!token){
+        throw new apiError(401,"no refresh token found in cookies")
+    }
+    const decodedToken=jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+    if(!decodedToken){
+        throw new apiError(401,"Invalid refresh token please login again")
+    }
+    const id=decodedToken._id
+    const user=await userModal.findById(id)
+    if(!user){
+        throw new apiError(501,"Invalid refresh token no user matched")
+    }
+    if(user.refreshToken!==token){
+        throw new apiError(501,"refresh token not matched with user's token")
+    }
+
+    const {refreshToken, accessToken}=generateTokens()
+
+    res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                accessToken,refreshToken
+            },
+            "new Access Token generated successfully"
+        )
+    )
+
+
 })
 
 export {
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -10,5 +10,6 @@ router.route("/login").post(loginUser)
 
 // secured routes
 router.route("/logout").post(verifyJWT ,logoutUser)
+router.route("/refresh_token").post(logoutUser)
 
 export default router
