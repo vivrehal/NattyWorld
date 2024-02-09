5be2b115615b427a9ff4c0f1ccb5a6979adbadf4
gargmanik6080 <gargmanik6080@gmail.com>
2024-02-09 21:49:09 +0530

    Added right side panel in diet page

--- client/src/components/sidebarItem.jsx
+++ client/src/components/sidebarItem.jsx
@@ -1,7 +1,9 @@
 const sidebarItem = (props) => {
 	return (
 		<div>
-			<div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+			<div onClick={() => {
+            activeIndex = props.index;
+        }} className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
 				<span className="text-[15px] ml-4 text-gray-200 font-bold">{props.name}</span>
 			</div>
 		</div>
--- client/src/pages/myDiets.jsx
+++ client/src/pages/myDiets.jsx
@@ -1,58 +1,49 @@
 import { useEffect, useState } from "react";
 import SidebarItem from "../components/sidebarItem.jsx";
 
-// const dietPlans = [
-// 	{
-// 		name: "Diet 1",
-// 		plan: "#"
-// 	},
-// 	{
-// 		name: "Diet 2",
-// 		plan: "#"
-// 	},
-// 	{
-// 		name: "Diet 3",
-// 		plan: "#"
-// 	},
-// ]
 const MyDiets = () => {
+	const [dietPlans, setDiet] = useState([]);
+	const [activeIndex, setIndex] = useState(0);
+	useEffect(() => {
+		const fetchDiet = async () => {
+			await fetch("http://localhost:9000/api/v1/diet/dietList")
+				.then(async (res) => {
+					let x = await res.json();
+					setDiet(x);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		};
 
-    const [dietPlans, setDiet] = useState([]);
-    // const Fetch = () => {
+		fetchDiet();
+	}, []);
+	// };
 
-    useEffect(() => {
-        const fetchDiet = async () => {
-            await fetch("http://localhost:9000/api/v1/diet/dietList")
-            .then(async (res) => {
-                // console.log(res.json())
-                let x = await res.json();
-                setDiet( x );
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-        }
+	var sidebarItems = dietPlans["data"]?.map((diet, index) => {
+		return <SidebarItem key={diet.name} name={diet.name} index={index} />;
+	});
 
-        fetchDiet();
-    }, []);
-    // };
-
-    var sidebarItems = dietPlans["data"]?.map((diet, index) => {
-        return <SidebarItem key={diet.name} name={diet.name} index={index} />;
-    });
 	return (
-		<div>
-			<div className="sidebar fixed top-10 bottom-0 mt-6 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
+		<div className="flex">
+			{/* SideBar */}
+			<div className="sidebar flex-box fixed top-10 bottom-0 mt-6 lg:left-0 w-[20%] overflow-y-auto text-center bg-black
+				border-r-2 border-white ">
 				<div className="text-gray-100 text-xl">
 					<div className="p-2.5 mt-1 flex items-center">
-						<i className="bi bi-egg bg-blue-600"></i>
-						<h1 className="font-bold text-gray-200 text-[18px] ml-3">My Diets</h1>
+						<h1 className="font-bold text-gray-200 text-[18px]">
+							My Diets
+						</h1>
 					</div>
-					<div className="my-2 bg-gray-600 h-[1px]"></div>
 				</div>
-
+				{/* Diet Plan's Names in the sidebar */}
 				{sidebarItems}
-				<div className="my-4 bg-gray h-[1px]">hdtyuh</div>
+			</div>
+			{/* Diet Display */}
+			<div className="flex-grow overflow-auto bg-gray-900 text-gray-200 text-[18px] w-[72%] h-[90vh]" style={{ marginLeft: "20%"}}>
+				{/* Content of the diet display */}
+				jhgfd3<br></br>
+				erfg
 			</div>
 		</div>
 	);
