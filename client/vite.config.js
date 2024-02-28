7cf9eeff97c609cb24dfbf554b6c32d391517dd9
gargmanik6080 <gargmanik6080@gmail.com>
2024-02-28 20:14:02 +0530

    server url updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api':'https://nattyworld-server.onrender.com'
+      '/api':'https://nattyworld-server.onrender.com:9000'
     },
   },
   plugins: [react()],
