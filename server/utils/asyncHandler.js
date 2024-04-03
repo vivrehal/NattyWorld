2a70b2bd3682f71ab5fd8d48e056ed5e4f2160be
gargmanik6080 <gargmanik6080@gmail.com>
2024-04-03 21:42:50 +0530

    Minor changes in asyncHandler.js response handling

--- server/utils/asyncHandler.js
+++ server/utils/asyncHandler.js
@@ -4,10 +4,10 @@ const asyncHandler = (fn) => async(req, res, next)=>{
         await fn(req,res,next)
     } catch (error) {
         if(!Number.isInteger(error.code)){
-            console.log("error: " +error.code + " end");
+            console.log("error: " +error.code );
             res.status(500).json({
                 success:false,
-                message:"Internal server error"
+                message:error.code
             })
         }
         else res.status(error.code || 500)?.json({
