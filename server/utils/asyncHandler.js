3790f7674e819266544426278a7a38f481fcb38a
gargmanik6080 <gargmanik6080@gmail.com>
2024-04-03 21:28:31 +0530

    error code bug handeled

--- server/utils/asyncHandler.js
+++ server/utils/asyncHandler.js
@@ -3,7 +3,14 @@ const asyncHandler = (fn) => async(req, res, next)=>{
     try {
         await fn(req,res,next)
     } catch (error) {
-        res.status(error.code || 500)?.json({
+        if(!Number.isInteger(error.code)){
+            console.log("error: " +error.code + " end");
+            res.status(500).json({
+                success:false,
+                message:"Internal server error"
+            })
+        }
+        else res.status(error.code || 500)?.json({
             success:false,
             message:error.message
         })
